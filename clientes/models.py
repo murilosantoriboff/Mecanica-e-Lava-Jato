fd91c429fb2676d5521e4ff2ef659040cabe9e86
Murilo Santori Boff <murilosantori45@gmail.com>
2022-09-20 10:06:36 -0300

    Finalizando as pré preenchimento de dados do forms

--- clientes/models.py
+++ clientes/models.py
@@ -1,9 +1,21 @@
 from django.db import models
 
 class Cliente(models.Model):
-    nome = models.CharField('')
-    sobrenome = 
-    cpf = 
+    nome = models.CharField(max_length=50)
+    sobrenome = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    cpf = models.CharField(max_length=11)
+
+    def __str__(self):
+        return self.nome
 
 class Carro(models.Model):
-    pass
+    carro = models.CharField(max_length=50)
+    placa = models.CharField(max_length=50)
+    ano = models.IntegerField()
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    lavagens = models.IntegerField(default=0)
+    consertos = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.carro
--- clientes/views.py
+++ clientes/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Cliente
+from .models import Carro
+import re
 
 def clientes(request):
     if request.method == 'GET':
@@ -11,5 +14,26 @@ def clientes(request):
         email = request.POST.get('email')
         cpf = request.POST.get('cpf')
         carros = request.POST.getlist('carro')
-        placa = request.POST.getlist('placa')
-        ano = request.POST.getlist('ano')
+        placas = request.POST.getlist('placa')
+        anos = request.POST.getlist('ano')
+
+        cliente = Cliente.objects.filter(cpf = cpf)
+
+        if cliente.exists():
+            return render(request, 'clientes.html', {'nome':nome, 'sobrenome':sobrenome, 'email':email, 'carros':zip(carros, placas, anos)})
+
+        if not re.fullmatch(re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+'), email):
+            return render(request, 'clientes.html', {'nome':nome, 'sobrenome':sobrenome, 'cpf':cpf, 'carros':zip(carros, placas, anos)})
+
+        cliente = Cliente(
+            nome = nome,
+            sobrenome = sobrenome,
+            email = email,
+            cpf = cpf
+        )
+
+        cliente.save()
+
+        for carro, placa, ano in zip(carros, placas, anos):
+            car = Carro(carro = carro, placa = placa, ano = ano, cliente = cliente)
+            car.save()
