3c3d8da69fb9626d66a7dabc39a0b1cfaaed12ab
Murilo Santori Boff <murilosantori45@gmail.com>
2022-09-20 08:32:51 -0300

    uptade

--- clientes/models.py
+++ clientes/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    nome = models.CharField('')
+    sobrenome = 
+    cpf = 
+
+class Carro(models.Model):
+    pass
