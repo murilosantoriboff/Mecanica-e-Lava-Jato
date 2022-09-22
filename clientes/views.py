15734288cba5ea10c96e9466407395a29611a466
Murilo Santori Boff <murilosantori45@gmail.com>
2022-09-22 20:17:53 -0300

    atualizando clientes: selecionando

--- clientes/views.py
+++ clientes/views.py
@@ -6,7 +6,8 @@ import re
 
 def clientes(request):
     if request.method == 'GET':
-        return render(request, 'clientes.html')
+        clientes_list = Cliente.objects.all()
+        return render(request, 'clientes.html', {'clientes':clientes_list})
 
     elif request.method == 'POST':
         nome = request.POST.get('nome')
@@ -37,3 +38,5 @@ def clientes(request):
         for carro, placa, ano in zip(carros, placas, anos):
             car = Carro(carro = carro, placa = placa, ano = ano, cliente = cliente)
             car.save()
+
+        return HttpResponse('teste')
